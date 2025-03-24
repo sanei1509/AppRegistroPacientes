f4ffe37ab1c2ff243490b6343ca85173431e3480
Santiago Neira <SN334109@fi365.ort.edu.uy>
2025-03-24 00:35:25 -0300

    Update Readme, upload screenshots of the app

---
+++ frontend/.vite/deps/react-hook-form.js
@@ -0,0 +1,30 @@
+import {
+  Controller,
+  Form,
+  FormProvider,
+  appendErrors,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+} from "./chunk-VN3R7NB6.js";
+import "./chunk-NVAVHLZZ.js";
+import "./chunk-5WRI5ZAA.js";
+export {
+  Controller,
+  Form,
+  FormProvider,
+  appendErrors,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+};
